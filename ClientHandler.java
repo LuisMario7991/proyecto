ddda045b854f951d27e9ec9733354198251406b7
Ricardo Mora <rurimoca@gmail.com>
2024-06-27 00:20:30 -0600

    Manejador de solicitudes del cliente

---
+++ ClientHandler.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+class ClientHandler implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            DHKeyExchange.ServerDH dhKeyExchange;
+            dhKeyExchange = new DHKeyExchange.ServerDH();
+            dhKeyExchange.exchangeKeys(Servidor.clientSocket);
+
+            UserManagement.authenticateUser();
+
+            // El servidor ahora espera por comandos
+            while (!Servidor.clientSocket.isClosed()) {
+                try {
+                    System.out.println("Esperando intrucción del cliente");
+                    String command = Servidor.dataInputStream.readUTF();
+                    CommandProcessor.processCommand(command);
+                } catch (IOException e) {
+                    System.err.println("Error al leer el comando: " + e.getMessage());
+                    Servidor.cerrarConexion();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
