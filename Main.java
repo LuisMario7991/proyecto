9e0439b8a0be5200871304fd1c5ef819bd305e62
Ricardo Mora <rurimoca@gmail.com>
2024-06-27 02:31:42 -0600

    Se cambia el manejo de excepciones

--- Main.java
+++ Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        try {
-            Servidor.startServer();
-            Servidor.waitForClient();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Servidor.startServer();
+        Servidor.waitForClient();
     }
 }
