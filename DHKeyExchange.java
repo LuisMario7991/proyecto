ac2549e6409edb3a0b4d49045dea443263b3df83
Ricardo Mora <rurimoca@gmail.com>
2024-07-01 02:37:21 -0600

    Envío de clave Diffie-Hellman al colaborador

--- DHKeyExchange.java
+++ DHKeyExchange.java
@@ -1,6 +1,3 @@
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
@@ -19,46 +16,42 @@ public class DHKeyExchange {
         private final PrivateKey privateKey;
 
         public ServerDH() throws Exception {
-            System.out.println("Generando parámetros Diffie-Hellman");
-            
             // Generar parámetros Diffie-Hellman
             AlgorithmParameterGenerator paramGen = AlgorithmParameterGenerator.getInstance("DH");
             paramGen.init(2048);
             AlgorithmParameters params = paramGen.generateParameters();
             this.dhSpec = params.getParameterSpec(DHParameterSpec.class);
-
+            
             // Generar par de claves Diffie-Hellman
             KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("DH");
             keyPairGen.initialize(dhSpec);
             this.keyPair = keyPairGen.generateKeyPair();
-
+            
             // Obtener clave pública y privada
             this.publicKey = keyPair.getPublic();
             this.privateKey = keyPair.getPrivate();
+            System.out.println("Parámetros Diffie-Hellman generados");
         }
 
-        public byte[] exchangeKeys(Socket clientSocket) throws Exception {
+        public byte[] exchangeKeys() throws Exception {
             System.out.println("Compartiendo parámetros Diffie-Hellman");
 
-            ObjectInputStream objectInputStream = new ObjectInputStream(clientSocket.getInputStream());
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(clientSocket.getOutputStream());
-
             // Enviar parámetros Diffie-Hellman a Alice
-            objectOutputStream.writeObject(this.dhSpec.getP());
-            objectOutputStream.flush();
-            objectOutputStream.writeObject(this.dhSpec.getG());
-            objectOutputStream.flush();
-            objectOutputStream.writeInt(this.dhSpec.getL());
-            objectOutputStream.flush();
+            Servidor.objectOutputStream.writeObject(this.dhSpec.getP());
+            Servidor.objectOutputStream.flush();
+            Servidor.objectOutputStream.writeObject(this.dhSpec.getG());
+            Servidor.objectOutputStream.flush();
+            Servidor.objectOutputStream.writeInt(this.dhSpec.getL());
+            Servidor.objectOutputStream.flush();
             System.out.println("Parámetros Diffie-Hellman enviados a Alice.");
 
             // Enviar clave pública a Alice
-            objectOutputStream.writeObject(this.publicKey);
-            objectOutputStream.flush();
+            Servidor.objectOutputStream.writeObject(this.publicKey);
+            Servidor.objectOutputStream.flush();
             System.out.println("Clave pública de Bob enviada a Alice.");
 
             // Recibir clave pública de Alice
-            PublicKey alicePublicKey = (PublicKey) objectInputStream.readObject();
+            PublicKey alicePublicKey = (PublicKey) Servidor.objectInputStream.readObject();
             if (!Utilidades.validatePublicKey(alicePublicKey)) {
                 throw new IllegalArgumentException("Clave pública recibida es inválida");
             }
@@ -77,7 +70,7 @@ public class DHKeyExchange {
             System.out.println("Clave compartida hash (Bob): " + Utilidades.bytesToHex(sharedSecretHash));
 
             // Guarda el hash en un archivo TXT
-            String fileName = "hasht.txt";
+            String fileName = "DHAESKEY.bin";
             Files.write(Paths.get(fileName), first16Bytes, StandardOpenOption.CREATE);
             // Files.writeString(Paths.get(fileName), bytesToHex(first16Bytes), StandardOpenOption.CREATE);
 
