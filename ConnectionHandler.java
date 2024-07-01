1e71b9b88716df317606510054f1af32f0cb6697
Ricardo Mora <rurimoca@gmail.com>
2024-07-01 02:41:51 -0600

    Se agrega clase para el manejo de peticiones del Colaborador

---
+++ ConnectionHandler.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+class ConnectionHandler implements Runnable {
+    byte[] key;
+
+    @Override
+    public void run() {
+        try {
+
+            // El servidor ahora espera por comandos
+            while (!Servidor.clientSocket.isClosed()) {
+                try {
+                    System.out.println("Esperando intrucción del cliente");
+                    String command = Servidor.dataInputStream.readUTF();
+                    CommandProcessor.processCommand(command);
+                } catch (IOException e) {
+                    System.out.println("Error al leer el comando: " + e.getMessage());
+                    Servidor.cerrarConexion();
+                }
+            }
+            Thread.sleep(10000);
+        } catch (Exception e) {
+            System.out.println("Error en el exchangeKeys(): " + e.getMessage());
+        }
+    }
+}
